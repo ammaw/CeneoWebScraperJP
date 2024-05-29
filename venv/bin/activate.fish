9e096f7336f4e8676001c5c3984a75ad02b126c1
Julia Petlic <jula@Julias-MacBook-Pro.local>
2024-05-29 20:07:57 +0200

    29-05-2024 JP

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/jula/Desktop/CeneoWebScraper/venv"
+set -gx VIRTUAL_ENV "/Users/jula/Desktop/111/CeneoWebScraperJP/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
